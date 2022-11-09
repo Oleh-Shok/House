7ba2e1e7ed68bbd9980e970acfe1376466a20822
Oleh-Shok <86318051+Oleh-Shok@users.noreply.github.com>
2022-11-09 19:02:06 +0200

    Add solution and project files

---
+++ ClassHouseWithAdressAndNickname/Program.cs
@@ -0,0 +1,57 @@
+﻿House house = new House();
+
+Console.WriteLine($"House address: {house.MyAddress}.");
+Console.WriteLine($"House nickname: {house.Nickname}.");
+Console.WriteLine($"Checking Nickname (if null write house address): {house.ToString()}.");
+Console.WriteLine($"Checking new address: {house.ChangedAddress}.");
+Console.WriteLine($"Comparison of two houses: {house.MyAddress.Equals(house.ChangedAddress)}.");
+
+
+class House
+{
+    public string MyAddress { get; private set; } = "Rivne, Bukovynska street";
+    
+    public string? Nickname { get; set; } = "Native home";
+
+    public string ChangedAddress
+    {
+        get => MyAddress;
+        set
+        {
+            if (MyAddress.Length < 5)
+            {
+                return;
+            }
+            else
+            {
+                MyAddress = value;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        if (!string.IsNullOrWhiteSpace(Nickname))
+        {
+            return Nickname;
+        }
+        else
+        {
+            return MyAddress;
+        }
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is House house)
+        {
+            return house.MyAddress == house.Nickname;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
+}
